d2771a8c4b7815e2daad9fcc2305395d02c20b03
Sky Scholfield <skyscholfield11@gmail.com>
2017-03-10 21:34:06 -0800

    add functionality to replace all integers with their corrisponding ping/pong values.

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,19 @@ $(document).ready(function() {
     event.preventDefault();
 
     var userInput = parseInt($("input#userNumber").val());
-    var results;
-    console.log(userInput);
-    $("#output").text(userInput);
+
+ for (userInput < 1; userInput--;) {
+  if (userInput % 15 === 0) {
+          $("#output").append("<li>" + "ping-pong" + "</li>");
+      } else if (userInput % 3 === 0) {
+          $("#output").append("<li>" + "ping" + "</li>");
+      } else if (userInput % 5 === 0) {
+          $("#output").append("<li>" + "pong" + "<li>");
+      } else {
+          $("#output").append("<li>" + userInput + "</li>");
+      }
+
+      console.log(userInput);
+    };
   });
 });
-
-for (var i = userInput; i < 0; i--) {
-  
-}
