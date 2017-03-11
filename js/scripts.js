85f26eb94500a6fc4cfbb689cc9b6742265c8242
Sky Scholfield <skyscholfield11@gmail.com>
2017-03-10 22:14:43 -0800

    add css styles.

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,20 @@ $(document).ready(function() {
   $("form#number").submit(function(){
     event.preventDefault();
 
-     var userInput = parseInt($("input#userNumber").val());
+    var userInput = parseInt($("input#userNumber").val());
 
- for (userInput < 1; userInput--;) {
-  if (userInput % 15 === 0) {
-          $("#output").append("<li>" + "ping-pong" + "</li>");
+    for (userInput >= 1; userInput--;) {
+      if (userInput % 15 === 0) {
+        $("#output").prepend("<li>" + "ping-pong" + "</li>");
       } else if (userInput % 3 === 0) {
-          $("#output").append("<li>" + "ping" + "</li>");
+        $("#output").prepend("<li>" + "ping" + "</li>");
       } else if (userInput % 5 === 0) {
-          $("#output").append("<li>" + "pong" + "<li>");
+        $("#output").prepend("<li>" + "pong" + "<li>");
       } else {
-          $("#output").append("<li>" + userInput + "</li>");
+        $("#output").prepend("<li>" + userInput + "</li>");
       }
 
-      $("#output").fadeIn(800);
+      $("#output").fadeIn(500);
       console.log(userInput);
     };
   });
